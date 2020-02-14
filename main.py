55507c5b24e93ee45b7bacaa1ff4a2cda10375a0
ololoshka2871 <SweetTreasure@2ch.hk>
2020-02-14 09:29:45 +0300

    Start - stop cycle, saving settings

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 #!/user/bin/env python
 # -*- coding: utf8 -*-
 
-import os
-import sys
-
-from collections import namedtuple
-
 from serial_enumerator import get_serial_ports
 
 from flask import Flask, render_template, redirect, url_for, request, send_from_directory
@@ -14,6 +9,7 @@ app = Flask(__name__)
 
 speeds = (9600, 38400, 57600, 115200)
 modes = ['8{}{}'.format(parity, stop) for parity in ('N', 'E', 'O') for stop in (1, 2)]
+last_error = "Сервер не запущен"
 
 settings = {
     "default_port": None,
@@ -33,16 +29,28 @@ settings = {
 @app.route('/', methods=['GET'])
 def index():
     ports = get_serial_ports()
-    return render_template('index.html', ports=ports, speeds=speeds, modes=modes, **settings)
+    return render_template('index.html', ports=ports, speeds=speeds, modes=modes, last_error=last_error, **settings)
 
 
 @app.route('/control', methods=['POST'])
 def control():
     global settings
+    global last_error
 
-    _settings = validate_server_settings(request.form)
+    if settings["server_is_running"] or len(request.form) == 0:
+        settings["server_is_running"] = False
+        last_error = None
+    else:
+        res = parse_form(request.form)
 
-    settings["server_is_running"] = not settings["server_is_running"]
+        if type(res) is str:
+            last_error = res
+            return redirect(url_for('index'))
+        else:
+            settings = res
+            settings["server_is_running"] = True
+            last_error = None
+
     return redirect(url_for('index'))
 
 
@@ -51,8 +59,35 @@ def serve_static(path):
     return send_from_directory('static', path)
 
 
-def validate_server_settings(form_values):
-    return True
+def parse_form(form_values):
+    new_settings = dict()
+
+    new_settings['default_port'] = form_values['serial_port']
+    if new_settings['default_port'] not in [p.device for p in get_serial_ports()]:
+        return 'Invalid device selected ({})'.format(new_settings['default_port'])
+
+    new_settings['default_speed'] = form_values['serial_speed']
+    if new_settings['default_speed'] not in [str(s) for s in speeds]:
+        return 'Invalid speed selected ({})'.format(new_settings['default_speed'])
+    else:
+        new_settings['default_speed'] = int(new_settings['default_speed'])
+
+    new_settings['default_mode'] = form_values['serial_mode']
+    if new_settings['default_mode'] not in modes:
+        return 'Invalid mode selected ({})'.format(new_settings['default_mode'])
+
+    try:
+        new_settings['default_use_rts'] = bool(form_values['serial_rts'])
+        new_settings['tcp_port_selector'] = int(form_values['tcp_port'])
+        new_settings['tcp_max_connections'] = int(form_values['tcp_max_connections'])
+        new_settings['rtu_max_trys'] = int(form_values['rtu_max_trys'])
+        new_settings['rtu_min_delay'] = int(form_values['rtu_min_delay'])
+        new_settings['rtu_rx_timeout'] = int(form_values['rtu_rx_timeout'])
+        new_settings['tcp_timeout'] = int(form_values['tcp_timeout'])
+    except Exception as e:
+        return str(e)
+
+    return new_settings
 
 
 if __name__ == "__main__":
